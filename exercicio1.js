11370cc1d89f1d5521a4e6ad008b5f35083b13a4
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-02-20 00:01:36 -0300

    mais exercicios

--- exercicio1.js
+++ exercicio1.js
@@ -1,69 +1,59 @@
-const box1 = [...document.querySelectorAll(".box_1")]
-const box2 = [...document.querySelectorAll(".box_2")]
-const box3 = [...document.querySelectorAll(".box_3")]
+const box = [...document.querySelectorAll(".box")]
 
-box_all = [box1, box2, box3]
 
-function clicked(evt){
-    box_all.map((e)=>{
-        if(e[0] == box1[0]){
-            // let id = evt.parentElement.id
-            let a = e
-            let r = Math.floor(Math.random()*3)
-            let b = box3[r]
-            console.log("w")
-            setTimeout(()=>{
-                 b.innerHTML="O"
-            }, 500)
-            let remover = box_all[0].splice(0,1)
+
+function clicked(id,evt){
+    box.map((e)=>{
+        if(e.parentElement.id == id){
+            let r = Math.floor(Math.random()*9)
+            let b = box[r]
+
+            if(b != evt && b.innerHTML == ""){        
+                setTimeout(()=>{
+                    b.innerHTML="O"
+                }, 700)    
+            }else{
+                for (let i of box){
+                    let evt_i = evt
+                    if(i.parentElement.id != evt_i.parentElement.id && i.innerHTML === ""){      
+                        setTimeout(()=>{
+                            i.innerHTML="O"
+                        }, 700)
+                        break;                  
+                    }        
+                }  
+            }
         }
-        //  if(box1[1] == evt){
-        //      console.log("s2")
-        //  }
-        //  if(box1[2] == evt){
-        //      console.log("s3")
-        //  }
-        //      // box1.map((e)=>{
-        //      // if(evt == e.parentElement.id){
-        //      //     let bo = box3[0]
-        //      //     console.log("op")
-        //      // }
-        //  box2.map((e)=>{
-                 
-        //  })
-        //  box3.map((e)=>{
-                 
-        //  })
-    
-    })
-    
+    }) 
 }
 
+function conferir(){
+    console.log("entrou")
+    box.map((e)=>{
+        let next=e.parentNode.nextSibling.nextSibling.firstChild
+        let prev=next.parentNode.previousSibling.previousSibling.previousSibling.previousSibling.firstChild
+       
+        console.log(next,e,prev)
+        // let prev=e.previousElementSibling
+        if(prev != null && next != null){
+            console.log("entrou2")
+            if((e.innerHTML == "X" && prev.innerHTML == "X" && next.innerHTML =="X")){
+                console.log("voce ganhou")
+            }
+        }
+        
+        
+    })
+}
 
-box1.map((e)=>{
+box.map((e)=>{
     e.addEventListener("click",(evt)=>{
         if(evt.target.innerHTML == ""){
             evt.target.innerHTML= "X"
             console.log(evt.target.parentElement.id)
-            clicked(evt.target)
-        }
-            
-})
-
-box2.map((e)=>{
-    e.addEventListener("click",(evt)=>{
-        if(evt.target.innerHTML == ""){
-            evt.target.innerHTML= "X"
-        }
-        clicked()
-})
-})
-box3.map((e)=>{
-    e.addEventListener("click",(evt)=>{
-        if(evt.target.innerHTML == ""){
-            evt.target.innerHTML= "X"
-        }
-        clicked()
+            let id = evt.target.parentElement.id
+            clicked(id ,evt.target)
+            conferir()
+        }        
     })
-})
 })
