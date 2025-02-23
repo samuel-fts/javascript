99dc130cc13b1d338183b8e70a7bd5f68a0e9762
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-02-23 16:13:37 -0300

    nova aula

--- aula92.js
+++ aula92.js
@@ -1,11 +1,18 @@
 import {cursos,getTodosCursos} from "./modulo92.js"
-import getTodosCursos,{cursos} from "./modulo92.js"// export padrao so pode ter um 
+import {getTodosCursos as c,cursos} from "./modulo92.js"// export padrao so pode ter um 
 import {carro} from "./modulo92.js"
+import c from "./modulo92.js"// posso colocar oçutro nome, sem o as. pq é o default
+// pode ser renomeado com o as
 
+import * as m_cursos from "./modulo92.js"//  importa todos os exports na msm variavel 
 
 
 console.log("rodando")
-console.log(cursos)
+console.log(m_curso.default())// mostra o curso default ou pode colocar o nome do export
 console.log(carro)
-console.log(getTodosCursos())
+console.log(c()) // é um metodo
+
+c.addCurso("http")
+c.apargarCurso()
+
 
--- modulo92.js
+++ modulo92.js
@@ -6,4 +6,26 @@ const getTodosCursos=()=>{
 }
 
 export {carro} // precisa ser exportado!!
-export default {getTodosCursos}
+export {getTodosCursos}
+
+
+class Curso {
+    static cursos=["javascript", "html", "css"]
+
+    static getTodosCursos=()=>{
+        return this.cursos
+    }
+
+    static getCursos=(i_cursos)=>{
+        return this.cursos[i_cursos]
+    }
+
+    static addCursos=()=>{
+        this.cursos.push(novoCurso)
+    }
+
+    static apagarCurso=()=>{
+       this.cursos=[] 
+    }
+}
+export default Curso 
