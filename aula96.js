9879021d43c6529032f8ec70ef66d913a200f368
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-02-26 17:04:54 -0300

    novas aulas

--- aula96.js
+++ aula96.js
@@ -1,6 +1,16 @@
 import c from "./contatos.js"
 const listaContatos=document.getElementById("listaContatos")
 const btn_gravar=document.getElementById("btn_gravar")
+const btn_remove=document.getElementById("btn_remove")
+
+btn_remove.addEventListener("click",(evt)=>{
+    let value={
+        nome:document.getElementById("f_nome").value,
+        telefone:document.getElementById("f_telefone").value,
+        email:document.getElementById("f_email").value,
+    }
+    c.removeContato(value)
+})
 
 btn_gravar.addEventListener("click",(evt)=>{
     const cont={
--- contatos.js
+++ contatos.js
@@ -1,4 +1,4 @@
-import { contatos } from "./bancoContatos,js";
+import { contatos } from "./bancoContatos.js";
 
 let contato={// um objeto
     getTodosContatos:function(){
@@ -7,25 +7,52 @@ let contato={// um objeto
     getContato:function(i_cont){
         return contatos[i_cont]
     },
+
+    removeContato:function(value){
+        contatos = contatos.filter((e)=>{
+            console.log(value.nome)
+            if(value.nome === e.nome && value.telefone === e.telefone && value.email === e.email){
+                  console.log("entropu auqwi")
+                return false
+            }else{
+                console.log("entropu auqw2i")
+                return true
+            }
+        })
+      
+        console.log(contatos)
+        
+    },
+
     addContato:function(novoContato,destinoDOM){
         const cont={
             nome:novoContato.nome,
             telefone:novoContato.telefone,
             email:novoContato.email
         }
+
+        
+        
+
         contatos.push(cont)
-        const div=document.createElement("div")
-        div.setAttribute("class", "contato")
-        const p_nome=document.createElement("p")
-        p_nome.innerHTML=novoContato.nome
-        const p_telefone=document.createElement("p")
-        p_nome.innerHTML=novoContato.telefone
-        const p_mail=document.createElement("p")
-        p_nome.innerHTML=novoContato.email
-        div.appendChild(p_nome)
-        div.appendChild(p_telefone)
-        div.appendChild(p_mail)
-        destinoDOM.appendChild(div)
+
+        destinoDOM.innerHTML=""
+
+        contatos.forEach((c)=>{
+            const div=document.createElement("div")
+            div.setAttribute("class", "contato")
+            const p_nome=document.createElement("p")
+            p_nome.innerHTML=c.nome
+            const p_telefone=document.createElement("p")
+            p_nome.innerHTML=c.telefone
+            const p_mail=document.createElement("p")
+            p_nome.innerHTML=c.email
+            div.appendChild(p_nome)
+            div.appendChild(p_telefone)
+            div.appendChild(p_mail)
+            destinoDOM.appendChild(div)
+        })
+        console.log(contatos)
     }
 }
 
