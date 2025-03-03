deb60e6762f1451d4bea9ac9a0f2874520b33b3c
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-03-03 17:30:30 -0300

    novas aulas

---
+++ aula117.js
@@ -0,0 +1,5 @@
+//os casos necessarios para o uso de ;
+let i=null ; i=0// códigos em sequencia
+let canal="cfb";// quando vc declara uma vaiavel e ja começa um codigo direto de um valor
+[1,2,3].map(()=>{})
+
