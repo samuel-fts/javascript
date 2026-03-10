2e83a4e0b79253d0a1553f4a84159b94b2d531dc
samuel <samuel.fts.s.8@gmail.com>
2026-03-10 09:36:52 -0300

    new

--- aula.js
+++
@@ -1,9 +0,0 @@
-let nome = new String("samuel")
-let no = new String("sa")
-console.log(typeof(nome))
-
-nome.charAt(0)
-nome.charCodeAt(0)
-nome.concat(no)
-nome.indexOf("u")
-nome.lastIndexOf("l")
